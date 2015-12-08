b55d96948893b25d6612bc121d7a08d1fb4966b6
HB <HB@HB-PC>
2015-12-08 11:06:09 +0900

    hnb 우리 프로젝트

--- hnb/src/main/java/com/hnb/main/MainController.java
+++ hnb/src/main/java/com/hnb/main/MainController.java
@@ -14,17 +14,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@Controller // servlet은 더이상 사용하지 않고 컨트롤러가 자동으로 매핑함
 public class MainController {
-	
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(Locale locale, Model model) {  // 모델은 request와 response를 모두 포함
+		logger.info("메인컨트롤러 home 메소드 진입 {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -33,7 +31,7 @@ public class MainController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "home"; // dispatcher와 response.getWriter().print()를 포함
+					   // home은 경로를 의미함
 	}
-	
 }
